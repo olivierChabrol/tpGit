9d410b62f85b21c61026d20b7e3933957371fb40
olivier chabrol <olivier.chabrol@univ-amu.fr>
2022-01-05 19:39:07 +0100

    Create adn.py

---
+++ tp1/adn.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    documentation here
+"""
+
+__author__ = 'Alain Connu'
+
+
+
+def is_valid(adn_str):
+    pass
+
+
+def get_valid_adn(prompt='chaîne : '):
+    pass
+
